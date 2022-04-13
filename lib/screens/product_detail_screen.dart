f246257320763f45f2ecd5cc9a327ba1b8305490
theoSPAS <spasovska.t@yahoo.com>
2022-04-13 22:15:25 +0200

    Optimizing the code

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -17,7 +17,7 @@ class ProductDetailScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
+            SizedBox(
               height: 300,
               width: double.infinity,
               child: Image.network(
